1237c9959a311891f55255361043014aaeda03c6
ArunManoj17 <arunmanoj17@gmail.com>
2017-01-04 18:24:13 +0530

    Add files via upload

---
+++ RemoteWebDriverTest.java
@@ -0,0 +1,115 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.html5.*;
+import org.openqa.selenium.logging.*;
+import org.openqa.selenium.remote.*;
+import org.openqa.selenium.Cookie.Builder;
+
+/**
+ * For programming samples and updated templates refer to the Perfecto GitHub at: https://github.com/PerfectoCode
+ */
+public class RemoteWebDriverTest {
+
+    public static void main(String[] args) throws MalformedURLException, IOException {
+        System.out.println("Run started");
+//System.getProperty("172.17.110.12");
+        String browserName = "mobileOS";
+        DesiredCapabilities capabilities = new DesiredCapabilities(browserName, "", Platform.ANY);
+        String host = "partners.perfectomobile.com";
+        capabilities.setCapability("user", "test5@tcs.com");
+        capabilities.setCapability("password", "Welcome1019");
+
+        //TODO: Change your device ID
+        capabilities.setCapability("deviceName", "02157DF2A1B46C22");
+//Map<String, ObjMap<String, Object> params12 = new HashMap<>();
+//params12.put("location", "1134,1591");
+//Object result12 = driver.executeScript("mobile:touch:tap", params12);
+
+//ect> params11 = new HashMap<>();
+//params11.put("location", "1152,1509");
+//Object result11 = driver.executeScript("mobile:touch:tap", params11);
+
+
+        System.getProperties().put("https.proxyHost", "172.17.110.12");
+        System.getProperties().put("https.proxyPort", "8080");
+        // Use the automationName capability to define the required framework - Appium (this is the default) or PerfectoMobile.
+        // capabilities.setCapability("automationName", "PerfectoMobile");
+
+        // Call this method if you want the script to share the devices with the Perfecto Lab plugin.
+        PerfectoLabUtils.setExecutionIdCapability(capabilities, host);
+
+        // Add a persona to your script (see https://community.perfectomobile.com/posts/1048047-available-personas)
+        //capabilities.setCapability(WindTunnelUtils.WIND_TUNNEL_PERSONA_CAPABILITY, WindTunnelUtils.GEORGIA);
+
+        // Name your script
+        // capabilities.setCapability("scriptName", "RemoteWebDriverTest");
+
+        RemoteWebDriver driver = new RemoteWebDriver(new URL("https://" + host + "/nexperience/perfectomobile/wd/hub"), capabilities);
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+
+        try {
+            // write your code here
+        driver.get("https://www.ultimatix.net");
+        
+switchToContext(driver, "WEBVIEW");
+driver.findElementByXPath("//*[@id=\"userName\"]").sendKeys("12345");
+
+switchToContext(driver, "WEBVIEW");
+driver.findElementByXPath("//*[@id=\"passWord\"]").sendKeys("Jan@1234");
+
+switchToContext(driver, "WEBVIEW");
+driver.findElementByXPath("//*[@class=\"btn login-button\"]").click();
+
+
+        
+        
+        	
+        	
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                // Retrieve the URL of the Single Test Report, can be saved to your execution summary and used to download the report at a later point
+                String reportURL = (String)(driver.getCapabilities().getCapability(WindTunnelUtils.SINGLE_TEST_REPORT_URL_CAPABILITY));
+
+                driver.close();
+
+                // In case you want to download the report or the report attachments, do it here.
+                // PerfectoLabUtils.downloadReport(driver, "pdf", "C:\\test\\report");
+                // PerfectoLabUtils.downloadAttachment(driver, "video", "C:\\test\\report\\video", "flv");
+                // PerfectoLabUtils.downloadAttachment(driver, "image", "C:\\test\\report\\images", "jpg");
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            driver.quit();
+        }
+
+        System.out.println("Run ended");
+    }
+
+    private static void switchToContext(RemoteWebDriver driver, String context) {
+        RemoteExecuteMethod executeMethod = new RemoteExecuteMethod(driver);
+        Map<String,String> params = new HashMap<String,String>();
+        params.put("name", context);
+        executeMethod.execute(DriverCommand.SWITCH_TO_CONTEXT, params);
+    }
+
+    private static String getCurrentContextHandle(RemoteWebDriver driver) {
+        RemoteExecuteMethod executeMethod = new RemoteExecuteMethod(driver);
+        String context =  (String) executeMethod.execute(DriverCommand.GET_CURRENT_CONTEXT_HANDLE, null);
+        return context;
+    }
+
+    private static List<String> getContextHandles(RemoteWebDriver driver) {
+        RemoteExecuteMethod executeMethod = new RemoteExecuteMethod(driver);
+        List<String> contexts =  (List<String>) executeMethod.execute(DriverCommand.GET_CONTEXT_HANDLES, null);
+        return contexts;
+    }
+}
